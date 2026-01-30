2239cd5d487f572c1891e0a876406082970c7689
Naren Narayanan P S <narenn4406@gmail.com>
2026-01-30 09:59:59 +0530

    Change Fibonacci series limit and output format
    Updated the Fibonacci series limit from 10 to 25 and changed the output format to print each term on a new line.

--- Hello.java
+++ Hello.java
@@ -1,11 +1,11 @@
 public class Hello {
   public static void main(String[] args) {
 
-    int n = 10, firstTerm = 0, secondTerm = 1;
+    int n = 25, firstTerm = 0, secondTerm = 1;
     System.out.println("Fibonacci Series till " + n + " terms:");
 
     for (int i = 1; i <= n; ++i) {
-      System.out.print(firstTerm + ", ");
+      System.out.print(firstTerm + "\n");
 
       // compute the next term
       int nextTerm = firstTerm + secondTerm;
